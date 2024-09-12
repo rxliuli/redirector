d41e7361ef4d5e60bfb60031110c2bb444b7f5aa
rxliuli <rxliuli@gmail.com>
2024-09-12 22:17:30 +0800

    feat: add dark mode toggle

--- src/entrypoints/options/App.svelte
+++ src/entrypoints/options/App.svelte
@@ -9,18 +9,21 @@
   import Dataset from './components/Dataset.svelte'
   import Form from './components/Form.svelte'
   import { StarIcon } from 'lucide-svelte'
+  import DarkMode from './components/DarkMode.svelte'
+  import { Button } from '$lib/components/ui/button'
 </script>
 
 <Card class="container my-8">
   <CardHeader>
     <CardTitle class="flex items-center gap-2">
-      <span class="text-xl font-bold">Redirector</span>
-      <a
-        href="https://github.com/rxliuli/redirector"
-        target="_blank"
-        class="ml-auto"
-      >
-        <StarIcon class="w-4 h-4 text-yellow-500" />
+      <span class="text-xl font-bold mr-auto">Redirector</span>
+      <DarkMode />
+      <a href="https://github.com/rxliuli/redirector" target="_blank">
+        <Button variant="outline" size="icon">
+          <StarIcon
+            class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all text-yellow-500"
+          />
+        </Button>
       </a>
     </CardTitle>
   </CardHeader>
