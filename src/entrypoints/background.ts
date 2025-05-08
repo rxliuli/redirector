773f6a0ff91425ede2c5f1e01aa2ff5f899ed9ae
rxliuli <rxliuli@gmail.com>
2025-05-08 14:53:31 +0800

    fix: prevent redirect loop detection from interfering with normal redirects

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -10,7 +10,6 @@ export default defineBackground(() => {
 
   browser.storage.sync.get('rules').then((data) => {
     rules = data.rules || []
-    console.log('Rules', rules)
   })
 
   browser.storage.sync.onChanged.addListener((changes) => {
@@ -19,12 +18,11 @@ export default defineBackground(() => {
     }
   })
 
-  const extensionRedirects = new Map<number, boolean>()
-  const redirectCounts = new Map<
-    number,
+  const redirected = new Map<
+    string,
     {
       count: number
-      lastTime: number
+      timestamp: number
     }
   >()
 
@@ -33,23 +31,25 @@ export default defineBackground(() => {
     if (!rule) {
       return {}
     }
-    const currentCount = redirectCounts.get(tabId) ?? { count: 0, lastTime: 0 }
-    if (Date.now() - currentCount.lastTime > 3000) {
-      currentCount.count = 0
-    }
-    if (currentCount.count >= 5) {
-      console.error(
-        `Circular redirect detection: tab ${url} has reached the maximum number of redirects, rule: ${JSON.stringify(
-          rule,
-        )}`,
-      )
-      return { cancel: true }
+    const data = redirected.get(url)
+    if (!data || Date.now() - data.timestamp > 3000) {
+      redirected.set(url, { count: 1, timestamp: Date.now() })
+    } else {
+      data.count++
+      redirected.set(url, data)
+      if (data.count >= 3) {
+        console.error(
+          `Circular redirect detection: url ${url} has reached the maximum number of redirects, rule: ${JSON.stringify(
+            rule,
+          )}`,
+        )
+        return {}
+      }
     }
     const redirectUrl = matchRule(rule, url).url
     if (redirectUrl === url) {
       return {}
     }
-
     console.log(
       '[webRequest] Redirecting from',
       url,
@@ -58,102 +58,47 @@ export default defineBackground(() => {
       'redirectUrl',
       redirectUrl,
     )
-    redirectCounts.set(tabId, {
-      lastTime: Date.now(),
-      count: currentCount.count + 1,
-    })
-    extensionRedirects.set(tabId, true)
     return { redirectUrl }
   }
 
-  async function gotoRedirectUrl(tabId: number, url: string) {
-    // console.log('[gotoRedirectUrl] Redirecting to', url)
-    await browser.tabs.update(tabId, { url })
+  async function handleRedirect(details: {
+    tabId: number
+    url?: string
+  }): Promise<{
+    redirectUrl?: string
+  }> {
+    if (details.tabId === -1 || !details.url) {
+      return {}
+    }
+    const { redirectUrl } = getRedirectUrl(details.tabId, details.url)
+    if (!redirectUrl) {
+      return {}
+    }
+    await browser.tabs.update(details.tabId, { url: redirectUrl })
+    return { redirectUrl }
   }
 
-  // TODO: https://developer.apple.com/forums/thread/735111
   if (!import.meta.env.SAFARI) {
+    // TODO: https://developer.apple.com/forums/thread/735111
     browser.webRequest.onBeforeRequest.addListener(
       async (details) => {
-        if (details.tabId === -1) {
-          return {}
-        }
-
-        const { redirectUrl, cancel } = getRedirectUrl(
-          details.tabId,
-          details.url,
-        )
-        if (cancel) {
-          return { cancel: true }
-        }
-        if (!redirectUrl) {
-          return {}
-        }
-        new Promise((resolve) => setTimeout(resolve, 10)).then(async () => {
-          await gotoRedirectUrl(details.tabId, redirectUrl)
-        })
-        return { cancel: true, redirectUrl }
+        return handleRedirect(details)
       },
       { urls: ['<all_urls>'], types: ['main_frame'] },
     )
-  }
-  browser.tabs.onRemoved.addListener((tabId) => {
-    extensionRedirects.delete(tabId)
-    redirectCounts.delete(tabId)
-  })
-  browser.webNavigation.onCommitted.addListener(async (details) => {
-    if (details.frameId === 0) {
-      const tabId = details.tabId
-      if (extensionRedirects.has(tabId)) {
-        extensionRedirects.delete(tabId)
-      } else {
-        redirectCounts.set(tabId, {
-          count: 0,
-          lastTime: Date.now(),
-        })
-      }
-      // TODO: https://developer.apple.com/forums/thread/727388
-      if (import.meta.env.SAFARI) {
-        const { redirectUrl, cancel } = getRedirectUrl(
-          details.tabId,
-          details.url,
-        )
-        if (cancel || !redirectUrl) {
-          return
-        }
-        await gotoRedirectUrl(details.tabId, redirectUrl)
-      }
-    }
-  })
-  if (!import.meta.env.SAFARI) {
     // TODO: https://developer.apple.com/documentation/safariservices/assessing-your-safari-web-extension-s-browser-compatibility#:~:text=onHistoryStateUpdated
     browser.webNavigation.onHistoryStateUpdated.addListener(async (details) => {
-      if (details.tabId === -1) {
-        return
-      }
-      const { redirectUrl, cancel } = getRedirectUrl(details.tabId, details.url)
-      if (cancel) {
-        return
-      }
-      if (!redirectUrl) {
-        return
-      }
-      await gotoRedirectUrl(details.tabId, redirectUrl)
+      await handleRedirect(details)
     })
   } else {
-    // https://stackoverflow.com/a/69938797/8409380
+    browser.webNavigation.onBeforeNavigate.addListener(async (details) => {
+      await handleRedirect(details)
+    })
     browser.tabs.onUpdated.addListener(async (tabId, _changeInfo, tab) => {
-      if (tabId === -1 || !tab.url) {
-        return
-      }
-      const { redirectUrl, cancel } = getRedirectUrl(tabId, tab.url)
-      if (cancel) {
-        return
-      }
-      if (!redirectUrl) {
-        return
-      }
-      await gotoRedirectUrl(tabId, redirectUrl)
+      await handleRedirect({
+        tabId,
+        url: tab.url,
+      })
     })
   }
 
